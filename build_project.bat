4fd23c89a512d4d7a656df354114753217251c4e
sanka-eu <sanyadimka@gmail.com>
2023-11-10 16:29:09 +0300

    some changes in build_project.bat

--- build_project.bat
+++ build_project.bat
@@ -1,5 +1,7 @@
+rmdir /s /q ".\build"
 mkdir build
 "C:\Program Files\CMake\bin\cmake.EXE" --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=C:\msys64\ucrt64\bin\gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=C:\msys64\ucrt64\bin\g++.exe "-S." "-B./build" -G Ninja
 cd build
 cmake --build .
-start .\src\application\app.exe
+cd ..
+start .\build\src\application\app.exe
